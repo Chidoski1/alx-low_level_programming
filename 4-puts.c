d089ec7bc05a8a25c8240af27f60dc25c930d8ff
Chidoski1 <chidieberenwankwo39@yahoo.com>
2022-03-11 11:50:04 -0800

    Puts.c

--- 4-puts.c
+++ 4-puts.c
@@ -16,8 +16,9 @@ int main(void)
   
 {
   
-  ("\"Programming is like building a multilingual puzzle")
-    
-    return (0);
+  puts("\"Programming is like building a multilingual puzzle");
+  
+  return (0);
   
 }
+
