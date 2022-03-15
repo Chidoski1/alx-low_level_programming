e57cbd4670654a5e083ae27c6cb7e2c6ec431010
Chidoski1 <chidieberenwankwo39@yahoo.com>
2022-03-15 16:13:14 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,22 @@
-#include "main.h"
+include "main.h"
+
 /**
- * main - entry block
- *@void: no argument
- * Return: 0;
- **/
+ * main - prints _putchar
+ *
+ * Return: 0 (Success)
+ */
+
 int main(void)
 {
-    char ermias[] = "_putchar";
-    int c;
-    for (c = 0; c < 8; c++);
-{
-            _putchar(ermias[c]);
-}
-_putchar('\n');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
