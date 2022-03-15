2438acd9aa5aa0125eec2bf0c66ab48a69686b6a
Chidoski1 <chidieberenwankwo39@yahoo.com>
2022-03-15 16:07:44 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,18 +2,16 @@
 /**
  * main - entry block
  *@void: no argument
- * Return: 0
+ * Return: 0;
  **/
 int main(void)
 {
     char ermias[] = "_putchar";
-
     int c;
-
-    for (c = 0; c < 8; c++)
-    {
+    for (c = 0; c < 8; c++);
+{
             _putchar(ermias[c]);
-    }
-    _putchar('\n');
-    return (0);
+}
+_putchar('\n');
+return (0);
 }
