e20cee9e4704a2bbdd23f56ad303449c6f556a0f
Chidoski1 <chidieberenwankwo39@yahoo.com>
2022-04-14 15:57:47 -0700

    Executable

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,7 +1,8 @@
-#ifndef _CALC_H_
-#define _CALC_H_
+#ifndef CALC_H
+#define CALC_H
 
-
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct op - Struct op
@@ -22,4 +23,4 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 
-#endif /* _CALC_H_ */
+#endif /*CALC_H*/
